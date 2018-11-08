964346dac3fbf7bd539310d407e9a922d4c5e7ed
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-11-08 19:58:26 -0200

    Multiple factions wee!

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const { createCanvas, loadImage, ImageData } = require('canvas')
 const RGBQuant = require('rgbquant')
 
 const quants = {
-  resistance: new RGBQuant({
+  res: new RGBQuant({
     colors: require('./palettes.json').resistance.length,
     palette: require('./palettes.json').resistance
+  }),
+  enl: new RGBQuant({
+    colors: require('./palettes.json').enlightened.length,
+    palette: require('./palettes.json').enlightened
   })
 }
 
-const canvasSize = 960
+const factions = {}
 
 if (!process.env.TELEGRAM_TOKEN) {
   console.error('TELEGRAM_TOKEN environment variable is missing. Can\'t start.')
@@ -20,31 +24,35 @@ const bot = new Telegram(process.env.TELEGRAM_TOKEN, { polling: true })
 
 bot.on('message', async (msg) => {
   if (msg.photo) {
-    // Initialize canvas
-    console.log(`Generating badge for ${msg.from.first_name} (${msg.from.username})...`)
-    const pictureCanvas = createCanvas(585, 585)
-    const pictureCtx = pictureCanvas.getContext('2d')
-
-    // Get the image, draw and reduce it
-    const { file_path } = await bot.getFile(msg.photo[msg.photo.length - 1].file_id)
-    const picture = await loadImage(`https://api.telegram.org/file/bot${process.env.TELEGRAM_TOKEN}/${file_path}`)
-    pictureCtx.drawImage(picture, 0, 0, 585, 585)
-
-    quants.resistance.sample(pictureCanvas)
-    quants.resistance.palette(true)
-    const reduced = new ImageData(new Uint8ClampedArray(quants.resistance.reduce(pictureCanvas)), 585)
-    pictureCtx.putImageData(reduced, 0, 0)
-
-    const finalCanvas = createCanvas(960, 960)
-    const finalCtx = finalCanvas.getContext('2d')
-    const frame = await loadImage('./frames/resistance.png')
-    finalCtx.drawImage(pictureCanvas, 125, 125, 710, 710)
-    finalCtx.drawImage(frame, 0, 0, 960, 960)
-
-    bot.sendPhoto(msg.chat.id, finalCanvas.toBuffer('image/jpeg', { quality: 1 }))
+    if (factions[msg.chat.id]) {
+      console.log(`Generating badge for ${msg.from.first_name} (${msg.from.username})...`)
+      const pictureCanvas = createCanvas(585, 585)
+      const pictureCtx = pictureCanvas.getContext('2d')
+      const { file_path } = await bot.getFile(msg.photo[msg.photo.length - 1].file_id)
+      const picture = await loadImage(`https://api.telegram.org/file/bot${process.env.TELEGRAM_TOKEN}/${file_path}`)
+      pictureCtx.drawImage(picture, 0, 0, 585, 585)
+      quants[factions[msg.chat.id]].sample(pictureCanvas)
+      quants[factions[msg.chat.id]].palette(true)
+      const reduced = new ImageData(new Uint8ClampedArray(quants[factions[msg.chat.id]].reduce(pictureCanvas)), 585)
+      pictureCtx.putImageData(reduced, 0, 0)
+      const finalCanvas = createCanvas(960, 960)
+      const finalCtx = finalCanvas.getContext('2d')
+      const frame = await loadImage(`./frames/${factions[msg.chat.id]}.png`)
+      finalCtx.drawImage(pictureCanvas, 125, 125, 710, 710)
+      finalCtx.drawImage(frame, 0, 0, 960, 960)
+      factions[msg.chat.id] = null
+      bot.sendPhoto(msg.chat.id, finalCanvas.toBuffer('image/jpeg', { quality: 1 }))
+    } else {
+      bot.sendMessage(msg.chat.id, 'You have to tell me the faction first. Type /res or /enl to choose!').catch(console.log)
+    }
   }
 })
 
 bot.onText(/\/start/, async (msg) => {
-  bot.sendMessage(msg.chat.id, 'Send me the picture you want to make a badge with.').catch(console.log)
+  bot.sendMessage(msg.chat.id, 'Type /res or /enl to get started!').catch(console.log)
+})
+
+bot.onText(/\/(res|enl)/, async (msg, match) => {
+  factions[msg.chat.id] = match[1]
+  bot.sendMessage(msg.chat.id, 'Great! Now send me the picture you want to make the badge with.').catch(console.log)
 })
